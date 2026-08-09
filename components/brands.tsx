8d08120a460e5a8608188c77d52cc7e4cbd2debe
Namit-ai <bhandulanamit@gmail.com>
2026-08-09 15:49:42 -0700

    Update brands.tsx

--- components/brands.tsx
+++ components/brands.tsx
@@ -7,357 +7,153 @@ import BrandCard from "./BrandCard";
 import ClientTooltip from "./ClientTooltip";
 
 export const clientsData = [
-  // AKRAYA
   {
     name: "Adobe",
     company: "Akraya",
     roles: [
-      { title: "Data Scientist", skills: ["AI/ML", "Langcharts", "Research"] },
-      {
-        title: "Product Marketing Manager",
-        skills: ["SAAS", "GTM", "Product Strategy", "Launch"],
-      },
-      {
-        title: "Content Marketing Manager",
-        skills: [
-          "Content Strategy",
-          "Brand Marketing",
-          "SEO",
-          "Designing",
-          "Landing Pages",
-          "Branding",
-        ],
-      },
-      {
-        title: "Data Analyst",
-        skills: ["Power BI", "Tableau", "Data analysis"],
-      },
-      {
-        title: "Customer Success Architect",
-        skills: ["AEM", "CJA"],
-      },
-      {
-        title: "Program/Project Manager",
-        skills: ["Product Launches", "Agile", "SDLC", "Enterprise clients"],
-      },
+      { title: "Data Scientist", skills: ["AI/ML", "LangChain", "Research"] },
+      { title: "Product Marketing", skills: ["SaaS", "GTM", "Product Strategy", "Launch"] },
+      { title: "Content Marketing", skills: ["Content Strategy", "Brand Marketing", "SEO", "Designing", "Landing Pages", "Branding Content"] },
+      { title: "Data Analyst", skills: ["Power BI", "Tableau", "Data Analysis"] },
+      { title: "Customer Success Architect", skills: ["AEM", "CJA"] },
+      { title: "Program/Project Manager", skills: ["Product Launches", "Agile", "SDLC", "Enterprise clients"] },
     ],
   },
   {
     name: "Visa",
     company: "Akraya",
     roles: [
-      {
-        title: "Talent Acquisition Manager",
-        skills: ["TA", "Workday", "High Volume Recruiting"],
-      },
-      {
-        title: "Recruiter",
-        skills: ["Early Careers", "End-to-End Recruitment", "Domain Specific"],
-      },
-      {
-        title: "Java Developer",
-        skills: ["Java/J2EE", "React", "Spring", "Microservices"],
-      },
-      {
-        title: "HR Employment Assistant",
-        skills: ["HR Operations", "IRS Filing", "Documentation", "Onboarding"],
-      },
-      {
-        title: "Project/Program Manager",
-        skills: [
-          "Finance Background",
-          "Point-of-Sale",
-          "ATM Infrastructure",
-          "Implementation",
-          "Reporting",
-        ],
-      },
-      {
-        title: "Full Stack Engineer",
-        skills: ["70% Backend", "30% Frontend", "Java", "React", "Spring"],
-      },
-      {
-        title: "Employee Relations Partner",
-        skills: [
-          "Performance Management",
-          "Coaching",
-          "Employee Relations",
-          "Employment Laws",
-        ],
-      },
+      { title: "Talent Acquision Manager", skills: ["TA", "Workday", "High Volume Recruiting"] },
+      { title: "Recruiter", skills: ["Early Careers", "End-to-End Recruitment", "Domain Specific client Experience"] },
+      { title: "Java Developer", skills: ["Java/J2EE", "React", "Spring", "Microservices"] },
+      { title: "HR Employment Assitant", skills: ["HR Operations", "IRS Filling", "Documentation", "Onboarding"] },
+      { title: "Project/Program Manager", skills: ["Finance Background", "Point-of-Sale", "ATM infrastructure", "Implementation", "Reporting"] },
     ],
   },
   {
     name: "Palo Alto Networks",
     company: "Akraya",
     roles: [
-      {
-        title: "Network Security Engineer",
-        skills: ["NGFW (Firewalls)", "Network Security"],
-      },
-      {
-        title: "Project/Program Manager",
-        skills: ["NPI", "NSI", "Product Launches", "GTM"],
-      },
-      {
-        title: "Sales Operations Analyst",
-        skills: [
-          "Sales Compensation",
-          "Partner Compensation",
-          "Incentive Plans",
-          "SOX",
-          "Salesforce (SFDC)",
-        ],
-      },
+      { title: "Network Security Engineer", skills: ["NGFW (Firewalls)", "Network security"] },
+      { title: "Project/Program Manager", skills: ["NPI", "NSI", "Product Launches", "GTM"] },
+      { title: "Sales Operations Analyst", skills: ["Sales compensation", "Partner compensation/relations", "Incentive Plan", "Sox", "Salesforce (SFDC)"] },
+      { title: "Employee Relations Partner", skills: ["Performance Management", "Coaching", "Employee Relations", "Employment laws"] },
+      { title: "Full Stack Engineer", skills: ["70% Backend", "30% Frontend", "Java", "react", "spring"] },
     ],
   },
   {
     name: "Anaplan",
     company: "Akraya",
     roles: [
-      {
-        title: "Coupa Admin",
-        skills: ["Coupa", "PLM", "Administrator"],
-      },
-      {
-        title: "Program Manager",
-        skills: ["NPI", "NSI", "Product Launches", "GTM"],
-      },
-      {
-        title: "Real Estate PM",
-        skills: [
-          "Real Estate",
-          "Workplace Strategy",
-          "Space Planning",
-          "IT/AV Infrastructure",
-          "Vendor Management",
-        ],
-      },
-      {
-        title: "Marketing Operations Manager – Global",
-        skills: [
-          "Content & Webinars",
-          "Integrated Campaigns",
-          "Event/Webinar Operations",
-          "Global Campaign Content",
-        ],
-      },
+      { title: "Coupa Admin", skills: ["Coupa", "PLM", "Administrator"] },
+      { title: "Program Manager", skills: ["NPI", "NSI", "Product Launches", "GTM"] },
+      { title: "Real Estate PM", skills: ["Real estate", "workplace strategy", "Space Planning", "IT/AV infrastructure", "and vendor management"] },
+      { title: "Marketing Operations Manager – Global Content & Webinars", skills: ["Integrated campaigns", "end to end event/webinar operations", "global campaign content"] },
     ],
   },
   {
     name: "eBay",
     company: "Akraya",
     roles: [
-      {
-        title: "Product Designer",
-        skills: [
-          "UI/UX Designing",
-          "Buyer-Seller Ecosystem",
-          "Ecommerce",
-          "Android/iOS/Desktops",
-        ],
-      },
-      {
-        title: ".Net Developer",
-        skills: [".Net", "C#", "Payment Integration"],
-      },
-      {
-        title: "Business Analyst",
-        skills: [
-          "API Integration",
-          "System Integration",
-          "Genesys Cloud",
-          "Data Reporting",
-        ],
-      },
-      {
-        title: "Seller Capital Program Manager",
-        skills: [
-          "Consultative Selling",
-          "Seller Outreach",
-          "Performance Tracking",
-          "Coaching",
-        ],
-      },
+      { title: "Product Designer", skills: ["UI/UX Designing", "Buyer-seller ecosystem", "Ecommerce", "Android/iOS/Desktops"] },
+      { title: ".Net Developer", skills: [".Net", "C#", "Payment integration (Stripe or any other)"] },
+      { title: "Business Analyst", skills: ["API integration", "System integration", "Genesys Cloud", "Data Reporting", "Documentation"] },
+      { title: "Seller Capital Program Manager", skills: ["Consultative Selling", "Seller Outreach & Pipeline Development", "Drive Loan Originations", "Strategy & Prioritization"] },
+      { title: "Operations Analyst", skills: ["market research", "competitive intelligence", "customer journey analysis", "and business operations across eCommerce", "logistics", "retail", "and fintech"] },
+      { title: "Software Engineer", skills: ["Java", "Rest API", "NoSql", "JavaScript/TypeScript"] },
     ],
   },
   {
     name: "Okta",
     company: "Akraya",
     roles: [
-      {
-        title: "Software Engineer",
-        skills: ["Node.js", "OAuth", "IAM", "Authenticators"],
-      },
-      {
-        title: "Product Marketing Manager",
-        skills: ["SAAS", "GTM", "Product Strategy", "Launch"],
-      },
-      {
-        title: "Business Data Analyst",
-        skills: ["Data Analysis", "SQL", "Tableau", "Power BI"],
-      },
+      { title: "Software Engineer Social IAM", skills: ["Node.JS (Heavy Backend)", "Social Plugin SDK", "IAM", ""] },
+      { title: "Software Engineer", skills: ["Golang", "Authenticators", "Oauth"] },
+      { title: "Product Marketing", skills: ["SaaS", "GTM", "Product Strategy", "Launch"] },
+      { title: "Business Data Analyst", skills: ["Data Analysis", "Visualisation", "SQL", "Tableau", "Power BI", "Salesforce", "Excel"] },
+      { title: "Global Campaign Manager, Okta Platform", skills: ["marketing campaigns", "launch", "B2B demand generation", "cybersecurity or Identity and Access Management (IAM) SaaS industry"] },
+      { title: "Partner Marketing Manager - Public Sector", skills: ["B2G Marketing (Expert)", "Campaign Execution (Advanced)", "Partner Management (Advanced)", "Event Planning (Advanced)", "Data Analysis (Intermediate)"] },
+      { title: "Software Engineer, Subscription Management", skills: ["Node.js (primary focus)", "payment systems (Stripe)", "subscription management"] },
     ],
   },
-
-  // eTEAM
   {
     name: "Pinterest",
     company: "eTeam",
     roles: [
-      {
-        title: "Software Engineer",
-        skills: ["Java", "REST API", "NoSQL", "JavaScript/TypeScript"],
-      },
-      {
-        title: "Product Marketing Manager",
-        skills: ["B2B", "GTM", "Product Strategy", "Launch"],
-      },
-      {
-        title: "Data Engineer",
-        skills: ["Data Engineering", "Presto", "Airflow", "Cloud"],
-      },
-      {
-        title: "Data Analyst",
-        skills: ["Data Analysis", "SQL", "Tableau", "Power BI", "Excel"],
-      },
+      { title: "Data Engineer", skills: ["Data Engineering", "Presto", "Airflow", "Cloud", "Social Media Industry"] },
+      { title: "Data Analyst", skills: ["Data Analysis", "Visualisation", "SQL", "Tableau", "Power BI", "Salesforce", "Excel"] },
+      { title: "Adminitrative Assistant", skills: ["Calendar Management", "C-suite", "Travel Management"] },
+      { title: "Marketing Manager", skills: ["SaaS", "GTM", "Demand Generation", "Launch", "Campaign Management"] },
+      { title: "ABX Marketing Manager", skills: ["GTM", "MarTech", "ABM campaign", "Marketing", "Salesforce"] },
+      { title: "Localization Project Manager", skills: ["Localization", "content", "Project management", "B2B SAAS Backround"] },
     ],
   },
   {
     name: "Okta",
     company: "eTeam",
     roles: [
-      {
-        title: "Software Engineer - Social IAM",
-        skills: ["Node.js", "Social Plugin SDK", "OAuth", "IAM"],
-      },
-      {
-        title: "Software Engineer - Golang",
-        skills: ["Golang", "Authenticators", "OAuth", "Backend"],
-      },
-      {
-        title: "Product Marketing Manager",
-        skills: ["SAAS", "GTM", "Product Strategy", "Launch"],
-      },
-    ],
-  },
-  {
-    name: "Visa",
-    company: "eTeam",
-    roles: [
-      {
-        title: "Talent Acquisition Manager",
-        skills: ["TA", "Workday", "High Volume Recruiting"],
-      },
-      {
-        title: "Backend Engineer",
-        skills: ["Java", "Spring Boot", "Microservices", "Payment Systems"],
-      },
-      {
-        title: "Business Analyst",
-        skills: [
-          "API Integration",
-          "System Integration",
-          "Data Analysis",
-          "Reporting",
-        ],
-      },
-      {
-        title: "Project/Program Manager",
-        skills: [
-          "Finance Background",
-          "Payment Infrastructure",
-          "Implementation",
-          "GTM",
-        ],
-      },
-      {
-        title: "Marketing Manager",
-        skills: ["SAAS", "GTM", "Demand Generation", "Campaign Management"],
-      },
+      { title: "Software Engineer Social IAM", skills: ["Node.JS (Heavy Backend)", "Social Plugin SDK", "IAM", ""] },
+      { title: "Software Engineer", skills: ["Golang", "Authenticators", "Oauth"] },
+      { title: "Product Marketing", skills: ["SaaS", "GTM", "Product Strategy", "Launch"] },
+      { title: "Business Data Analyst", skills: ["Data Analysis", "Visualisation", "SQL", "Tableau", "Power BI", "Salesforce", "Excel"] },
+      { title: "Global Campaign Manager, Okta Platform", skills: ["marketing campaigns", "launch", "B2B demand generation", "cybersecurity or Identity and Access Management (IAM) SaaS industry"] },
+      { title: "Partner Marketing Manager - Public Sector", skills: ["B2G Marketing (Expert)", "Campaign Execution (Advanced)", "Partner Management (Advanced)", "Event Planning (Advanced)", "Data Analysis (Intermediate)"] },
+      { title: "Software Engineer, Subscription Management", skills: ["Node.js (primary focus)", "payment systems (Stripe)", "subscription management"] },
     ],
   },
   {
     name: "Salesforce",
     company: "eTeam",
     roles: [
-      {
-        title: "Software Engineer",
-        skills: ["Apex", "Lightning", "Salesforce Cloud", "Integrations"],
-      },
-      {
-        title: "Salesforce Admin",
-        skills: ["Salesforce Administration", "Customization", "Reporting"],
-      },
-      {
-        title: "Product Marketing Manager",
-        skills: ["SAAS", "GTM", "Product Strategy", "Launch"],
-      },
+      { title: "Salesforce Lightning Developer", skills: ["Salesforce", "Lightning Web Components (LWC)", "JavaScript", "HTML", "and CSS"] },
+      { title: "Salesforce Consultants", skills: ["Salesforce Marketing Cloud", "Sales cloud", "Service Cloud", "Commerce Cloud"] },
     ],
   },
   {
-    name: "Logitech",
+    name: "Trinet",
     company: "eTeam",
     roles: [
-      {
-        title: "Partner Marketing Manager - Public Sector",
-        skills: [
-          "B2G Marketing",
-          "Campaign Execution",
-          "Partner Management",
-          "Event Planning",
-        ],
-      },
-      {
-        title: "Global Campaign Manager",
-        skills: [
-          "Campaign Strategy",
-          "Global Execution",
-          "Content Creation",
-          "Analytics",
-        ],
-      },
+      { title: "Python Developer", skills: ["Python", "numpy", "pandas", "Visualization"] },
+      { title: "Payroll Admin/Manager", skills: ["Payroll", "compensation", "Administration"] },
+      { title: "HRBP", skills: ["HR", "Strategy Planning", "Performance tracking", "Coaching"] },
+    ],
+  },
+  {
+    name: "Visa",
+    company: "eTeam",
+    roles: [
+      { title: "Talent Acquision Manager", skills: ["TA", "Workday", "High Volume Recruiting"] },
+      { title: "Recruiter", skills: ["Early Careers", "End-to-End Recruitment", "Domain Specific client Experience"] },
+      { title: "Java Developer", skills: ["Java/J2EE", "React", "Spring", "Microservices"] },
+      { title: "HR Employment Assitant", skills: ["HR Operations", "IRS Filling", "Documentation", "Onboarding"] },
+      { title: "Project/Program Manager", skills: ["Finance Background", "Point-of-Sale", "ATM infrastructure", "Implementation", "Reporting"] },
     ],
   },
   {
     name: "Zillow",
     company: "eTeam",
     roles: [
-      {
-        title: "Software Engineer",
-        skills: ["Java", "AWS", "Spring", "Microservices"],
-      },
-      {
-        title: "Sales Enablement Manager",
-        skills: [
-          "Sales Enablement",
-          "Content Creation",
-          "Real Estate Domain",
-          "Training",
-        ],
-      },
-      {
-        title: "Accounting Manager",
-        skills: ["CPA", "Revenue Recognition", "ASC 606", "Big 4 Experience"],
-      },
-      {
-        title: "Director of SOX",
-        skills: ["SOX Compliance", "Revenue Recognition", "Internal Controls"],
-      },
+      { title: "Software Engineer", skills: ["Java", "AWS Step Functions", "EC2", "Spring"] },
+      { title: "Sales Enablement", skills: ["Sales enablement", "pitches", "Material", "Real estate Sales"] },
+      { title: "Director of SOX", skills: ["SOX compliance", "ASC-606", "CPA"] },
+      { title: "Accounting Manager", skills: ["CPA", "Big  4 experience", "Revenue Recognition", "ASC 606"] },
+      { title: "Data Annotation", skills: ["NLP", "LLM", "Annotations"] },
     ],
   },
   {
     name: "Thales",
     company: "eTeam",
     roles: [
-      {
-        title: "System Engineer",
-        skills: ["Linux", "Public Trust Clearance", "System Engineering"],
-      },
-      {
-        title: "System Administrator",
-        skills: ["Linux Administration", "Active Clearance", "Ticketing"],
-      },
+      { title: "System Engineer", skills: ["Linux", "Public Trust clearance", "System engineering"] },
+      { title: "System Admin", skills: ["Linux", "Ticketing", "Active Clearance"] },
+    ],
+  },
+  {
+    name: "Logitech",
+    company: "eTeam",
+    roles: [
+      { title: "Partner Marketing Manager - Public Sector", skills: ["B2G Marketing", "Campaign Execution", "Partner Management", "Event Planning", "Data Analysis"] },
+      { title: "Global Campaign Manager, Okta Platform", skills: ["marketing campaigns", "launch", "B2B demand generation", "Tech industry"] },
     ],
   },
 ];
